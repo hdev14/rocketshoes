3fd31799a7d9b6277d90e371b40dcdfe75b669e3
HermersonDev <ne.hermerson@gmail.com>
2020-03-11 11:40:21 -0300

    :tada: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'react/state-in-constructor': [ 'warn' ,'never'],
+    'react/static-property-placement': [ 'warn' ,'static public field'],
+    'import/prefer-default-export': 'off',
+  },
+};
