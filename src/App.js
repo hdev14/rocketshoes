5750096c1f06d773a1a1db27dfe1a2dc54fe760d
HermersonDev <ne.hermerson@gmail.com>
2020-03-13 09:18:28 -0300

    :construction: reactotron configs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './configs/ReactotronConfig';
+
 import store from './store';
 
 import GlobalStyle from './styles/global';
