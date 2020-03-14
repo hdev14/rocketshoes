63c7f5d9aaed6e140db56d8c07b6f76d0346cc60
HermersonDev <ne.hermerson@gmail.com>
2020-03-14 10:27:28 -0300

    :construction: adds sagas ont the redux

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,6 +1,13 @@
-export function addToCart(product) {
+export function addToCartRequest(id) {
   return {
-    type: '@cart/ADD_PRODUCT',
+    type: '@cart/ADD_PRODUCT_REQUEST',
+    id,
+  };
+}
+
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_PRODUCT_SUCCESS',
     product,
   };
 }
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case '@cart/ADD_PRODUCT':
+    case '@cart/ADD_PRODUCT_SUCCESS':
       return produce(state, (draftState) => {
         const productIndex = draftState.findIndex((p) => p.id === action.product.id);
 
